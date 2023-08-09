f0207d9c4b83a5a9e4f510c4ec0317d68c36848a
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2023-08-09 17:31:15 -0300

    Finalizado estilo home (não responsivo)

--- sustenapp-web-site/src/components/Home/index.jsx
+++ sustenapp-web-site/src/components/Home/index.jsx
@@ -1,6 +1,11 @@
 import './style.css'
 import '../../assets/logo-sustenapp.png'
 import homeImage from '../../assets/home-image.png'
+import grafico from '../../assets/grafico.png'
+import bolhas from '../../assets/bolhas.png'
+import floatBolhas from '../../assets/float-bolhas.png'
+
+import { DownloadSimple } from 'phosphor-react'
 
 // COMPONENTS
 
@@ -12,10 +17,20 @@ export default function Home() {
                 <h1> A casa do futuro <br /> no <span> presente </span> </h1>
                 <p> Controlamento e Gerenciamento dos <br /> gastos de energia e água </p>
          
-                <img src={homeImage} />
+                <div className="position-image">
+                    <img src={homeImage} />
+                    <img src={grafico} />
+                </div>
+
+                <a className='download' href="/"> <DownloadSimple className="downloadIcon" size={30} color="#ffff" weight="fill" /><span> DOWNLOAD APP</span> </a>
+
         </section> 
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1B44B0" fill-opacity="1" d="M0,160L60,138.7C120,117,240,75,360,101.3C480,128,600,224,720,256C840,288,960,256,1080,245.3C1200,235,1320,245,1380,250.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+        <div class="wave-container"></div>
+
+        <img className="bolhas2" src={bolhas} />
+        <img className="bolhas" src={bolhas} />
+        <img className='float-bolhas' src={floatBolhas} />
 </>
     )
 }
