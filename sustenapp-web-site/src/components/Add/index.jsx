d93653cafe0fa8cdf3c2e9388ec0f19bddf99a81
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2023-08-19 12:00:53 -0300

    Ajustado responsivisidade em Add

--- sustenapp-web-site/src/components/Add/index.jsx
+++ sustenapp-web-site/src/components/Add/index.jsx
@@ -14,7 +14,7 @@ export default function Add() {
             <div>
                 <h1> Com o app <span> simplifique </span> sua vida como nunca! </h1>
                 <p>Com o SustenApp, você pode saber o valor das contas antes mesmo de recebê-las, de maneira simples e automática.</p>
-                <div>
+                <div className="flex-link">
                     <a href="#" className='downloadLink'><GooglePlayLogo className='downloadAppIcon' size={25} color="white" weight="bold" />  DOWNLOAD APP</a>
                     <a href="#" className='feedbackDownload'><Star size={25} className='downloadAppIcon' color="black" weight="bold" />  Ver avaliações</a>
                 </div>
